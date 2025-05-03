2e8e23c9673eba039bfdcd222c2c89f185c12a9e
Faridul Reza Sagor <hiiamsea0@gmail.com>
2025-05-03 18:47:22 +0600

    logging webhook body

--- src/app/api/telegram/[id]/route.js
+++ src/app/api/telegram/[id]/route.js
@@ -7,7 +7,7 @@ import Block, { BlockType } from "@/models/Block";
 import Automation from "@/models/Automation";
 
 const getUniqueSubscribers = (subscribers) => {
-  const exists= new Set();
+  const exists = new Set();
   const uniqueSubscribers = [];
 
   for (const subscriber of subscribers) {
@@ -18,11 +18,10 @@ const getUniqueSubscribers = (subscribers) => {
   }
 
   return uniqueSubscribers;
-
-}
+};
 export async function POST(request, { params }) {
+  const body = await request.json();
   try {
-  
     if (!params.id) {
       return NextResponse.json({ error: "ID is required" }, { status: 400 });
     }
@@ -31,7 +30,6 @@ export async function POST(request, { params }) {
       _id: params.id,
     });
 
-
     if (!account) {
       return NextResponse.json(
         { error: "Telegram account not found" },
@@ -39,7 +37,6 @@ export async function POST(request, { params }) {
       );
     }
 
-    const body = await request.json();
     const text = body.message.text;
     if (!text) {
       return NextResponse.json(
@@ -49,12 +46,14 @@ export async function POST(request, { params }) {
     }
 
     if (text === "/subscribe") {
-      const subscribers = getUniqueSubscribers(
-        [...account.subscribers, body.message.chat])
+      const subscribers = getUniqueSubscribers([
+        ...account.subscribers,
+        body.message.chat,
+      ]);
 
       await TelegramAccount.findOneAndUpdate(
         { _id: params.id },
-        {  subscribers }, 
+        { subscribers }
       );
       await sendTelegramMessage(
         account.token,
@@ -66,15 +65,13 @@ export async function POST(request, { params }) {
         { status: 200 }
       );
     }
- 
+
     const block = await Block.findOne({
       type: BlockType.TelegramRecieveMessage,
       "data.accountId": params.id,
-    })
+    });
 
-
-
-    if(!block){
+    if (!block) {
       await sendTelegramMessage(
         account.token,
         body.chat.id,
@@ -87,34 +84,34 @@ export async function POST(request, { params }) {
       );
     }
 
-    const automation = await Automation.findById(block.data.automationId,{_id:1, status:1});
-    if(!automation || automation.status !== "active") {
+    const automation = await Automation.findById(block.data.automationId, {
+      _id: 1,
+      status: 1,
+    });
+    if (!automation || automation.status !== "active") {
       return NextResponse.json(
         { error: "Automation is not active" },
         { status: 200 }
       );
     }
 
-
-
-    await fetch(process.env.TASK_SERVER_URL,{
+    await fetch(process.env.TASK_SERVER_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        initial_state:{
+        initial_state: {
           text: text,
           message_type: "text",
         },
         automation_id: block.data.automationId.toString(),
         start_block_id: block._id.toString(),
       }),
-    })
+    });
 
-    return NextResponse.json({ok: true}, { status: 200 });
+    return NextResponse.json({ ok: true }, { status: 200 });
   } catch (error) {
-    const body = await request.json();
     console.log("body", JSON.stringify(body));
     console.error("Error in Telegram webhook route:", error);
     return NextResponse.json({ error: error.message }, { status: 500 });
